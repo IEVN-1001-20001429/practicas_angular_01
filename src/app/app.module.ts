67c56f971d96ea30f31f75772fe12f6456904142
Luis Angel Calvillo Leon <20001429@alumnos.utleon.edu.mx>
2023-10-04 23:15:53 -0600

    04/10/2023 - LACL - Cinepolis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MultiplicacionaxbComponent } from './formularios/multiplicacionaxb/multiplicacionaxb.component';
+import { MaterialModule } from './material/material/material.module';
+import { CinepolisComponent } from './formularios/cinepolis/cinepolis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OperasBasComponent,
-    MultiplicacionaxbComponent
+    MultiplicacionaxbComponent,
+    CinepolisComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatInputModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formularios/cinepolis/cinepolis.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis',
+  templateUrl: './cinepolis.component.html',
+  styleUrls: ['./cinepolis.component.css']
+})
+export class CinepolisComponent {
+  nombre:string = '';
+  cantidadComp:number = 0;
+  ifTarjeta:number = 0;
+  numBoletos:number = 0;
+
+  valorPagar:number = 0;
+  valorString:string = '';
+
+  procesarCompra(){
+    let totalBoletos = this.cantidadComp*7;
+    if(this.numBoletos<=totalBoletos){
+      if(this.numBoletos>5){
+        this.valorPagar = (this.numBoletos * 12000 * 0.85);
+      }else if(this.numBoletos >= 3 && this.numBoletos <= 5){
+        this.valorPagar = (this.numBoletos * 12000 * 0.90);
+      }else if(this.numBoletos<3){
+        this.valorPagar = (this.numBoletos * 12000);
+      }
+      if(this.ifTarjeta == 1){
+        this.valorPagar = this.valorPagar * 0.90;
+      }
+    }else if(this.numBoletos>totalBoletos){
+      alert("El limite de boletos comprados por persona es de 7, se excedieron, favor de cambiar la cantidad");
+    }
+  }
+
+  recargar(){
+    location.reload();
+  }
+}
